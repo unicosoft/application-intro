d3d1ad085745071a8e696b3516ed7983e3f341c5
Sebastian Kors <skors@unicosoft.de>
2015-09-17 13:02:38 +0200

    add global db config

---
+++ config/autoload/db.global.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'db' => [
+        'driver'         => 'PdoMysql',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ]
+    ],
+];
